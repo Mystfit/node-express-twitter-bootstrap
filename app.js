f83717bc9654118590b421e803fcd096a52f0b4b
Byron <byron@g-04-412-imac.mddn.vuw.ac.nz>
2012-11-13 21:25:28 +1300

    Node express server now running

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
-var app = express.createServer();
+var http = require('http');
 
-exports.init = function(port) {
+var app = express();
+var server = http.createServer();
+
+var locals = {
+        title:       'NodeJS Boostrap using Express / EJS / Twitter Bootstrap / CSS3',
+        description: 'Node Express HTML5 & CSS3',
+        author:      'Alexandre Collin'
+    };
+
+var init = function(port) {
 
     app.configure(function(){
     	app.set('views', __dirname + '/views');
@@ -10,6 +19,7 @@ exports.init = function(port) {
     	app.use(express.methodOverride());
     	app.use(express.static(__dirname + '/static'));
     	app.use(app.router);
+        app.use(function(err, req, res, next){ res.render('500.ejs', { locals: { error: err },status: 500 }); });
     	app.enable("jsonp callback");
     });
 
@@ -22,14 +32,26 @@ exports.init = function(port) {
 	   app.use(express.errorHandler()); 
     });
 
-
-    app.error(function(err, req, res, next){
-	   res.render('500.ejs', { locals: { error: err },status: 500 });	
-    });
-
     app.listen(port);
 
-    console.log("Listening on port %d in %s mode", app.address().port, app.settings.env);
+    console.log("Listening on port %d in %s mode", port, app.settings.env);
 
     return app;
 }
+
+app.get('/', function(req,res){
+
+    locals.date = new Date().toLocaleDateString();
+
+    console.log("Request recieved");
+
+    res.render('template.ejs', locals);
+    //res.send("Hello world");
+});
+
+/* The 404 Route (ALWAYS Keep this as the last route) */
+app.get('/*', function(req, res){
+    res.render('404.ejs', locals);
+});
+
+init(4000);
